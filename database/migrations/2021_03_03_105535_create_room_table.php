8c462a7f23e6404af3045a6989b158e16abb38f0
lukman <nandes88.ni@gmail.com>
2021-03-03 18:18:27 +0700

    menambahkan laravel breeze

---
+++ database/migrations/2021_03_03_105535_create_room_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room', function (Blueprint $table) {
+            $table->id();
+            $table->enum('category', ['living_room', 'children_room', 'decoration_room', 'bed_room']);
+            $table->bigInteger('price');
+            $table->longText('about_product');
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->string('image4');
+            $table->string('image5');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
