e71eccf5bd7a14c163dbeeefa0de0dd1abb27d7e
lukman <nandes88.ni@gmail.com>
2021-03-14 23:22:54 +0700

    refaktor dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,36 +1,28 @@
 const searchCategory = document.getElementById("search_category");
 const tbody = document.getElementsByTagName("tbody")[0];
+
+function httpRequest(url) {
+    fetch(url)
+        .then((response) => response.text())
+        .then((success) => {
+            if (success) {
+                tbody.innerHTML = success;
+            } else {
+                tbody.innerHTML = `
+            <tr>
+                <td colspan="8">Data rooms not found</td>
+            </tr>`;
+            }
+        })
+        .catch((responseError) => console.log(responseError));
+}
 // search category di change
 searchCategory.addEventListener("change", function (category) {
     const valueCategory = category.target.value;
     if (valueCategory === "all_data_rooms") {
-        fetch("/dashboard/ajax/all-data-rooms")
-            .then((response) => response.text())
-            .then((success) => {
-                if (success) {
-                    tbody.innerHTML = success;
-                } else {
-                    tbody.innerHTML = `
-                    <tr>
-                        <td colspan="8">Data rooms not found</td>
-                    </tr>`;
-                }
-            })
-            .catch((responseError) => console.log(responseError));
+        httpRequest("/dashboard/ajax/all-data-rooms");
     } else {
-        fetch(`/dashboard/ajax/${valueCategory}`)
-            .then((response) => response.text())
-            .then((success) => {
-                if (success) {
-                    tbody.innerHTML = success;
-                } else {
-                    tbody.innerHTML = `
-                    <tr>
-                        <td colspan="8">Data rooms not found</td>
-                    </tr>`;
-                }
-            })
-            .catch((responseError) => console.log(responseError));
+        httpRequest(`/dashboard/ajax/${valueCategory}`);
     }
 });
 
@@ -40,30 +32,8 @@ searchNamePrice.addEventListener("keyup", function () {
     const valueProductPrice = this.value;
     const valueCategory = searchCategory.value;
     if (valueCategory === "all_data_rooms") {
-        fetch(`/dashboard/ajax/all-data-rooms/${valueProductPrice}`)
-            .then((response) => response.text())
-            .then((success) => {
-                if (success) {
-                    tbody.innerHTML = success;
-                } else {
-                    tbody.innerHTML = `
-                    <tr>
-                        <td colspan="8">Data rooms not found</td>
-                    </tr>`;
-                }
-            });
+        httpRequest(`/dashboard/ajax/all-data-rooms/${valueProductPrice}`);
     } else {
-        fetch(`/dashboard/ajax/${valueCategory}/${valueProductPrice}`)
-            .then((response) => response.text())
-            .then((success) => {
-                if (success) {
-                    tbody.innerHTML = success;
-                } else {
-                    tbody.innerHTML = `
-                <tr>
-                    <td colspan="8">Data rooms not found</td>
-                </tr>`;
-                }
-            });
+        httpRequest(`/dashboard/ajax/${valueCategory}/${valueProductPrice}`);
     }
 });
