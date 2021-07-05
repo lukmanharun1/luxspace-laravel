8b14e85fcd39980bb8e69a5e71cb70b95c6f62c5
lukman <nandes88.ni@gmail.com>
2021-07-05 17:07:12 +0700

    menambahkan halaman 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>error</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.struktur-html')
+@section('title', 'Luxspace ~ Saingan IKEA')
+@push('include-css')
+{{-- using tailwindcss --}}
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+  
+@endpush
+@section('content')
+    <div class="flex justify-center items-center flex-col">
+        <img
+            src="images/design/logo.svg"
+            alt="Luxspace ~ Adalah sebuah website yang menjual barang-barang kece"
+            class="mt-6"
+          />
+        <img src="{{ asset('images/content/404_illustration.png') }}" alt="404 illustration ~ Luxspace" />
+        <h3 class="text-2xl md:text-3xl mt-10" style="color: #1F2B4A;">Opps! What's This?</h3>
+        <p class="md:text-xl text-lg mt-4 text-center" style="color: #828282;">
+            Don’t panic and put yourself <br> 
+            together because this page means <br> 
+            doesn’t exist anymore
+        </p>
+        <a href="/" class="bg-pink-400 text-black hover:bg-black hover:text-pink-400 rounded-full px-8 py-3 inline-block flex-none transition duration-200 mt-7">Back To Home</a>
+    </div>
+@endsection
